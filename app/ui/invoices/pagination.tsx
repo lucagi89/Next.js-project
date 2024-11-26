8fa510b50db042adb1093891c6abfe2da495e911
Luca Gattamelata <luca.gattamelata.dev@gmail.com>
2024-11-26 18:12:10 -0300

    handling errors

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -43,7 +43,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
             return (
               <PaginationNumber
                 key={page}
-                href={createPageURL(page)}
+                href={createPageURL(Number(page))}
                 page={page}
                 position={position}
                 isActive={currentPage === page}
